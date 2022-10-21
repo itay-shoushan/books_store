ac4dee9b9cabca34d5b180b3547f5fbc62925bad
Itay Shoushan <eitai999@gmail.com>
2022-10-21 13:26:47 +0300

    remove_strict_mode

--- frontend/src/Components/BooksArea/BookList/BookList.tsx
+++ frontend/src/Components/BooksArea/BookList/BookList.tsx
@@ -3,37 +3,52 @@ import { Navigate } from "react-router-dom";
 import BooksModel from "../../../models/books";
 import BooksServices from "../../../services/bookServices";
 import BookCard from "../BookCard/BookCard";
-import { useNavigate } from 'react-router-dom'
-import {useTimeout} from 'usehooks-ts'
-import {BookAddAction,BookClearAction,BookOverrideAllBooks,} from "../Redux/BooksRedux";
+import { useNavigate } from "react-router-dom";
+import { useInterval, useTimeout } from "usehooks-ts";
+import {
+  BookAddAction,
+  BookClearAction,
+  BookOverrideAllBooks,
+} from "../Redux/BooksRedux";
 import { store } from "../Redux/store";
 import "./BookList.css";
 
 function BookList(): JSX.Element {
   const [render, setRender] = useState(false);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const getBooksFromApi = (nav:boolean=false) => {
+    BooksServices.fetchAllBooks()
+      .then((apiBooks) => {
+        // setBooks(apiBooks);
+        // books.map(book => store.dispatch(BookAddAction(JSON.stringify(book))));
+        store.dispatch(BookOverrideAllBooks(apiBooks));
+        console.log("after fetch " + store.getState().BookState.books.length);
+        setRender(true);
+        if(nav) {
+          console.log("before navigate");
+          navigate("/BooksList");
+          console.log("after navigate");
+        }
+      })
+      .catch((err) => console.error(err.message));
+  };
+
+  useInterval(() => {
+    console.log("timeout");
+    // store.dispatch(BookClearAction());
+    getBooksFromApi(true);
+
+    //console.log(      "in useInterval fun. " + store.getState().BookState.books.length);
+    
+  }, 1000 * 60 * 0.5);
 
   useEffect(() => {
     if (store.getState().BookState.books.length > 0) {
       console.log("data available " + store.getState().BookState.books.length);
     } else {
       console.log("No data, fetching...");
-      BooksServices.fetchAllBooks()
-        .then((apiBooks) => {
-          // setBooks(apiBooks);
-          // books.map(book => store.dispatch(BookAddAction(JSON.stringify(book))));
-          store.dispatch(BookOverrideAllBooks(apiBooks));
-          useTimeout(() => {
-            console.log("timeout");
-            store.dispatch(BookClearAction());
-            console.log(
-              "in timeout fun. " + store.getState().BookState.books.length);
-            navigate("/BooksList");
-        }, 1000 * 60 * 0.5);
-          console.log("after fetch " + store.getState().BookState.books.length);
-          setRender(true);
-        })
-        .catch((err) => console.error(err.message));
+      getBooksFromApi();
     }
   }, []);
 
